0c8e136e4cb50a311a9b7296ae471b68b4c7c14d
Crowdbotics <team@crowdbotics.com>
2024-11-06 19:21:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "disposable-fitness--49926",
+  url: "https://disposable-fitness--49926.botics.co",
+  api: "https://disposable-fitness--49926.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
